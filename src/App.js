813eba0376fa74b9d4ff3e145bf5b92df96fb05c
ZoroIK <shreeshshrestha881@gmail.com>
2024-12-08 23:42:35 -0500

    Changes

--- src/App.js
+++ src/App.js
@@ -1,91 +1,105 @@
-// Import dependencies
-import React, { useRef, useEffect, useCallback } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import * as tf from "@tensorflow/tfjs";
 import Webcam from "react-webcam";
 import "./App.css";
-//import { nextFrame } from "@tensorflow/tfjs";
+import { nextFrame } from "@tensorflow/tfjs";
 
-import {drawRect} from "./utilities"; 
+import { drawRect } from "./utilities";
 
 function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
+  const [devices, setDevices] = useState([]);
+  const [selectedDeviceId, setSelectedDeviceId] = useState("");
+
+  // Get the list of video input devices
+  useEffect(() => {
+    const getDevices = async () => {
+      const deviceList = await navigator.mediaDevices.enumerateDevices();
+      const videoDevices = deviceList.filter((device) => device.kind === "videoinput");
+      setDevices(videoDevices);
+      if (videoDevices.length > 0) {
+        setSelectedDeviceId(videoDevices[0].deviceId);
+      }
+    };
+
+    getDevices();
+  }, []);
 
   // Main function
-  const runCoco = useCallback (async () => {
-    
-    //https://tesnsorflowjsrealtimemodel.s3.us-east.cloud-object-storage.appdomain.cloud/model.json
-    const net = await tf.loadGraphModel('https://tesnsorflowjsrealtimemodel.s3.us-east.cloud-object-storage.appdomain.cloud/model.json')
-    
-    //  Loop and detect hands
+  const runCoco = async () => {
+    const net = await tf.loadGraphModel(
+      "https://tesnsorflowjsrealtimemodel.s3.us-east.cloud-object-storage.appdomain.cloud/model.json"
+    );
+
     setInterval(() => {
       detect(net);
     }, 16.7);
-
-    
-  },[]);
-
-  
+  };
 
   const detect = async (net) => {
-    // Check data is available
     if (
       typeof webcamRef.current !== "undefined" &&
       webcamRef.current !== null &&
       webcamRef.current.video.readyState === 4
     ) {
-      // Get Video Properties
       const video = webcamRef.current.video;
-      const videoWidth = webcamRef.current.video.videoWidth;
-      const videoHeight = webcamRef.current.video.videoHeight;
+      const videoWidth = video.videoWidth;
+      const videoHeight = video.videoHeight;
 
-      // Set video width
       webcamRef.current.video.width = videoWidth;
       webcamRef.current.video.height = videoHeight;
 
-      // Set canvas height and width
       canvasRef.current.width = videoWidth;
       canvasRef.current.height = videoHeight;
 
-      // Make Detections
-      const img = tf.browser.fromPixels(video)
-      const resized = tf.image.resizeBilinear(img, [640,480])
-      const casted = resized.cast('int32')
-      const expanded = casted.expandDims(0)
-      const obj = await net.executeAsync(expanded)
-      console.log(obj)
+      const img = tf.browser.fromPixels(video);
+      const resized = tf.image.resizeBilinear(img, [640, 480]);
+      const casted = resized.cast("int32");
+      const expanded = casted.expandDims(0);
+      const obj = await net.executeAsync(expanded);
 
-      const boxes = await obj[1].array()
-      const classes = await obj[2].array()
-      const scores = await obj[4].array()
-      
-      // Draw mesh
+      const boxes = await obj[1].array();
+      const classes = await obj[2].array();
+      const scores = await obj[4].array();
+
       const ctx = canvasRef.current.getContext("2d");
 
-      // drawSomething(obj, ctx)  
-      requestAnimationFrame(()=>{drawRect(boxes[0], classes[0], scores[0], 0.8, videoWidth, videoHeight, ctx)}); 
-      
-      //delete previous images for storage 
-      tf.dispose(img)
-      tf.dispose(resized)
-      tf.dispose(casted)
-      tf.dispose(expanded)
-      tf.dispose(obj)
+      requestAnimationFrame(() => {
+        drawRect(boxes[0], classes[0], scores[0], 0.8, videoWidth, videoHeight, ctx);
+      });
 
+      tf.dispose(img);
+      tf.dispose(resized);
+      tf.dispose(casted);
+      tf.dispose(expanded);
+      tf.dispose(obj);
     }
   };
 
-  //useEffect(()=>{runCoco()},[]);
   useEffect(() => {
     runCoco();
-  }, [runCoco]);
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
+        <select
+          onChange={(e) => setSelectedDeviceId(e.target.value)}
+          value={selectedDeviceId}
+          style={{ position: "absolute", top: 10, zIndex: 10 }}
+        >
+          {devices.map((device) => (
+            <option key={device.deviceId} value={device.deviceId}>
+              {device.label || `Camera ${device.deviceId}`}
+            </option>
+          ))}
+        </select>
+
         <Webcam
           ref={webcamRef}
-          muted={true} 
+          muted={true}
+          videoConstraints={{ deviceId: selectedDeviceId }}
           style={{
             position: "absolute",
             marginLeft: "auto",
