35c5253a60e78ffcde34671fc9079c095e6a0696
ZoroIK <shreeshshrestha881@gmail.com>
2024-12-08 23:45:04 -0500

    Fixed changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useRef, useState, useEffect } from "react";
 import * as tf from "@tensorflow/tfjs";
 import Webcam from "react-webcam";
 import "./App.css";
-import { nextFrame } from "@tensorflow/tfjs";
+
 
 import { drawRect } from "./utilities";
 
